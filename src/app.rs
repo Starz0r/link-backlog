b7a44509055313210e2ad893dbc18f12da190a3c
Starz0r <starz0r@starz0r.com>
2022-03-23 01:27:41 -0500

    app: register openid handlers

--- src/app.rs
+++ src/app.rs
@@ -20,29 +20,29 @@ use {
     tracing::{info, trace},
 };
 
-type OpenIDClient = openid::Client<openid::Discovered, openid::StandardClaims>;
+pub type OpenIDClient = openid::Client<openid::Discovered, openid::StandardClaims>;
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(rename_all = "camelCase")]
-struct User {
-    id: String,
-    login: Option<String>,
-    first_name: Option<String>,
-    last_name: Option<String>,
-    email: Option<String>,
-    image_url: Option<String>,
-    activated: bool,
-    lang_key: Option<String>,
-    authorities: Vec<String>,
+pub struct User {
+    pub id: String,
+    pub login: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub email: Option<String>,
+    pub image_url: Option<String>,
+    pub activated: bool,
+    pub lang_key: Option<String>,
+    pub authorities: Vec<String>,
 }
 
-struct Session {
-    user: User,
-    token: openid::Token,
-    userinfo: Userinfo,
+pub struct Session {
+    pub user: User,
+    pub token: openid::Token,
+    pub userinfo: Userinfo,
 }
 
-type Sessions = DashMap<String, Session>;
+pub type Sessions = DashMap<String, Session>;
 
 pub(crate) struct Application {
     cfg: super::config::Configuration,
@@ -56,7 +56,22 @@ pub(crate) struct Application {
 impl Application {
     pub async fn prepare(config: super::config::Configuration) -> Result<Self, Error> {
         let tera = Arc::new(Tera::new("static/templates/**/*.html.tera")?);
+        let openid_client = Arc::new(
+            openid::DiscoveredClient::discover(
+                config.openid.client_id.clone(),
+                config.openid.client_secret.clone(),
+                Some(config.openid.redirect.clone()),
+                config.openid.issuer.clone(),
+            )
+            .await?,
+        );
+        trace!("OpenID Config: {:?}", openid_client.config());
+        let sessions = Arc::new(DashMap::new() as Sessions);
+        let apis = Router::new()
+            .route("/auth/oauth2/code/oidc", get(super::api::authenticate))
+            .route("/oauth2/login/oidc", get(super::api::login));
         let router = Router::new()
+            .nest("/api/v0", apis)
             .route("/", get(super::pages::index))
             .nest(
                 "/static",
@@ -70,18 +85,9 @@ impl Application {
                 ),
             )
             .layer(CookieManagerLayer::new())
-            .layer(Extension(tera.clone()));
-        let openid_client = Arc::new(
-            openid::DiscoveredClient::discover(
-                config.openid.client_id.clone(),
-                config.openid.client_secret.clone(),
-                Some(config.openid.redirect.clone()),
-                config.openid.issuer.clone(),
-            )
-            .await?,
-        );
-        trace!("OpenID Config: {:?}", openid_client.config());
-        let sessions = Arc::new(DashMap::new() as Sessions);
+            .layer(Extension(tera.clone()))
+            .layer(Extension(openid_client.clone()))
+            .layer(Extension(sessions.clone()));
         Ok(Self {
             cfg: config,
             addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 3030),
