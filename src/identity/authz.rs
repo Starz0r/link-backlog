e5c08e17986708bae47685081fbb186652bd3581
Starz0r <starz0r@starz0r.com>
2022-06-02 19:55:27 -0500

    identity: find the api key from the database

--- src/identity/authz.rs
+++ src/identity/authz.rs
@@ -1,6 +1,15 @@
 use std::sync::Arc;
 
-use crate::app::{Sessions, User};
+use {
+    sea_orm::{entity::prelude::*, DatabaseConnection},
+    tracing::warn,
+};
+
+use crate::{
+    app::{Sessions, User},
+    database::entity::api_keys,
+    pages::apikeys::APIKey,
+};
 
 pub fn user_from_session(sessions: Arc<Sessions>, token: String) -> Option<User> {
     let entry = match sessions.get(&token) {
@@ -10,4 +19,29 @@ pub fn user_from_session(sessions: Arc<Sessions>, token: String) -> Option<User>
 
     let (_, session) = entry.pair();
     Some(session.user.clone())
+}
+
+pub async fn valid_api_key(dbconn: Arc<DatabaseConnection>, key: String) -> Option<APIKey> {
+    // TODO: tokio::select between a hashmap cache and the current database pull
+    match api_keys::Entity::find()
+        .filter(api_keys::Column::Key.eq(key))
+        .one(dbconn.as_ref())
+        .await
+    {
+        // TODO: verify key is valid by checking the deleted_at timestamp
+        Ok(maybe_apikey) => match maybe_apikey {
+            Some(apikey_der) => match APIKey::from_inactive_der(apikey_der) {
+                Ok(apikey) => Some(apikey),
+                Err(e) => {
+                    warn!("api key from database could not be converted into rust repr: {e}");
+                    None
+                }
+            },
+            None => None,
+        },
+        Err(e) => {
+            warn!("database search for api key failed: {e}");
+            None
+        }
+    }
 }
